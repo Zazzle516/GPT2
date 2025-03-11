b125b9e1f8cc171a715774c6433dceb768e8e999
Zazzle <2405677060@qq.com>
2025-03-12 00:36:32 +0800

    update

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,12 +3,14 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-
+# 首先 从理论上 GPT-2 是 decoder-only model
+# 相比于原始 Transformer 有两个改动
+    # 1. LayerNorm 的计算位置
+    # 2. 在最后的 SelfAttention Block 中额外添加了 layer normalization
 
 @dataclass
 class GPTConfig:
-    # Q: 这些参数对应的什么 ?
-    block_size: int = 256
+    block_size: int = 256       # ?
     vocab_size: int = 65        # 上下文长度
     n_embd:  int = 384          # 每个 token 的特征维度数
 
@@ -22,6 +24,7 @@ class GPT(nn.Module):
         super().__init__()
         self.config = config
 
+        # 完成 GPT-2 的网络骨架  这些 API 有的涉及计算有的不涉及  但是统一通过 forward 来调用
         # nn.ModuleDict() 是什么
         # nn.Embedding(): 存放 Embedding 矩阵   param1: batch_size  param2: sequence_length
         self.transformer = nn.ModuleDict(dict(
